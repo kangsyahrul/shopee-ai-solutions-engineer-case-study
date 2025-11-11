f6d20089100f8146ad107214e73cb99aed49da2b
Syahrul MBP <syahrulh@xl.co.id>
2025-11-11 16:20:44 +0700

    Setup Qdrant locally

---
+++ qdrant.sh
@@ -0,0 +1,7 @@
+# Pull the latest Qdrant Docker image
+docker pull qdrant/qdrant
+
+# Run Qdrant container with persistent storage
+docker run -p 6333:6333 -p 6334:6334 \
+    -v "$(pwd)/qdrant_storage:/qdrant/storage:z" \
+    qdrant/qdrant
